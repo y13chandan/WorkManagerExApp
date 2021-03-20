f2ec50a306021d483504ac9e4ed5856742030606
chandan <chandan.singh@industrybuying.com>
2021-03-20 16:33:55 +0530

    added the notification method and worker class

--- app/src/main/java/com/example/workmanagerexapp/SendNotificationWorker.kt
+++ app/src/main/java/com/example/workmanagerexapp/SendNotificationWorker.kt
@@ -1,12 +1,43 @@
 package com.example.workmanagerexapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
-class SendNotificationWorker(context: Context,  workParams: WorkerParameters) : Worker(context, workParams) {
 
+class SendNotificationWorker(context: Context, workParams: WorkerParameters) : Worker(
+    context,
+    workParams
+) {
+
+
+    /*
+    * responsible for doing work so we put the work here that is need to be performed.
+    * to display a notification i am calling displayNotification method
+     */
     override fun doWork(): Result {
-        TODO("Not yet implemented")
+        displayNotification("WorkManager", "Finished with work")
+    }
+
+    private fun displayNotification(title: String, task: String) {
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "workmanagerexample",
+                "workmanagerexample",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notification = NotificationCompat.Builder(applicationContext, "workmanagerexample")
+            .setContentTitle(title)
+            .setContentText(task)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+        notificationManager.notify(1, notification.build())
     }
 }
